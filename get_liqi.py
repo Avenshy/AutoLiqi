c5f1bb3adee792f63978c48bc24d96e0e42e0b2c
Avenshy <30952834+Avenshy@users.noreply.github.com>
2024-03-30 19:12:15 +0800

    update get code.js

--- get_liqi.py
+++ get_liqi.py
@@ -5,7 +5,7 @@ Headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/53
 
 def get_version():
     req = requests.get('https://game.maj-soul.com/1/version.json',headers=Headers)
-    return req.json()['version']
+    return req.json()
 
 def get_prefix(version):
     req = requests.get( f'https://game.maj-soul.com/1/resversion{version}.json',headers=Headers)
@@ -15,16 +15,23 @@ def get_liqi(prefix):
     req = requests.get( f'https://game.maj-soul.com/1/{prefix}/res/proto/liqi.json',headers=Headers)
     return req.text
 
+def get_code_js(code):
+    req = requests.get( f'https://game.maj-soul.com/1/{code}',headers=Headers)
+    return req.text
 def main():
     version = get_version ()
-    prefix = get_prefix(version)
+    code_js = get_code_js(version["code"])
+    prefix = get_prefix(version['version'])
     liqi =  get_liqi(prefix)
     with open('liqi.json','w') as f:
         f.write(liqi)
+    with open('code.js','w') as f:
+        f.write(code_js)
     env = os.getenv('GITHUB_ENV')
     with open(env, "a") as f:
-        f.write(f"version={version} - {prefix}")
-
+        f.write(f"code-js=v{version['version']}")
+    with open(env, "a") as f:
+        f.write(f"liqi-json={prefix}")
 
 if __name__ == '__main__':
     main()
